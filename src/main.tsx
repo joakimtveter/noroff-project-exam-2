8f65ca345c2ee045f8795ac48f160b29b7423431
Joakim Tveter <joakim@tveter.net>
2023-08-30 14:30:32 +0200

    Initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <h1>Hello</h1>
+    </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+const publicDir = resolve(__dirname, 'src/public');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    root,
+    plugins: [react()],
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': root,
+        },
+    },
+});
