960d9d344036925a0693ccd75a99da897cd83742
Joakim Tveter <joakim@tveter.net>
2023-10-12 22:38:41 +0200

    Fixing issues for deployment

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -34,64 +34,66 @@ export default function SignUp() {
     };
 
     return (
-        <Container component='main' maxWidth='xs'>
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>
-                <Typography component='h1' variant='h4'>
-                    Sign up
-                </Typography>
-                <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                autoComplete='given-name'
-                                name='name'
-                                required
-                                fullWidth
-                                id='name'
-                                label='Name'
-                                autoFocus
-                            />
+        <Layout>
+            <Container component='main' maxWidth='xs'>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
+                    <Typography component='h1' variant='h4'>
+                        Sign up
+                    </Typography>
+                    <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete='given-name'
+                                    name='name'
+                                    required
+                                    fullWidth
+                                    id='name'
+                                    label='Name'
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id='email'
+                                    label='Email Address'
+                                    name='email'
+                                    autoComplete='email'
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name='password'
+                                    label='Password'
+                                    type='password'
+                                    id='password'
+                                    autoComplete='new-password'
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                id='email'
-                                label='Email Address'
-                                name='email'
-                                autoComplete='email'
-                            />
+                        <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent='flex-end'>
+                            <Grid item>
+                                <Link component={RouterLink} to='/sign-in' variant='body2'>
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name='password'
-                                label='Password'
-                                type='password'
-                                id='password'
-                                autoComplete='new-password'
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-                        Sign Up
-                    </Button>
-                    <Grid container justifyContent='flex-end'>
-                        <Grid item>
-                            <Link component={RouterLink} to='/sign-in' variant='body2'>
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        </Layout>
     );
 }
