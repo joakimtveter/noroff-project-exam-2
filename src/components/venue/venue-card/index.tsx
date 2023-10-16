88af118b6484ec3a3db36fbf5aabe787afa5cbd8
Joakim Tveter <joakim@tveter.net>
2023-10-16 09:23:07 +0200

    Fixed all venues layout

--- src/components/venue/venue-card/index.tsx
+++ src/components/venue/venue-card/index.tsx
@@ -1,10 +1,16 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+import { visuallyHidden } from '@mui/utils';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { Box, Button, CardActionArea, CardActions } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
-import { visuallyHidden } from '@mui/utils';
+
 import VenuePlaceholder from '@/assets/venue-placeholder.svg';
 
 interface VenueCardProps {
@@ -21,31 +27,33 @@ export default function VenueCard(props: VenueCardProps) {
         navigate(`/venues/${id}`);
     };
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea onClick={handleClick}>
-                <CardMedia
-                    component='img'
-                    sx={{ aspectRatio: '1/1', objectFit: 'cover' }}
-                    image={media[0] ?? VenuePlaceholder}
-                    alt={name}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant='h5' component='div'>
-                        {name}
-                    </Typography>
-                    <Typography variant='body2' color='text.secondary'>
-                        {description}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button component={Link} to={`/venues/${id}`} size='small' sx={{ color: 'text.green' }}>
-                    View
-                    <Box component={'span'} style={visuallyHidden}>
-                        {' ' + name}
-                    </Box>
-                </Button>
-            </CardActions>
-        </Card>
+        <Grid item component='li'>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea onClick={handleClick}>
+                    <CardMedia
+                        component='img'
+                        sx={{ aspectRatio: '1/1', objectFit: 'cover' }}
+                        image={media[0] ?? VenuePlaceholder}
+                        alt={name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant='h5' component='div'>
+                            {name}
+                        </Typography>
+                        <Typography variant='body2' color='text.secondary'>
+                            {description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button component={Link} to={`/venues/${id}`} size='small' sx={{ color: 'text.green' }}>
+                        View
+                        <Box component={'span'} style={visuallyHidden}>
+                            {' ' + name}
+                        </Box>
+                    </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     );
 }
--- src/pages/venues/index.tsx
+++ src/pages/venues/index.tsx
@@ -1,7 +1,11 @@
-import Layout from '@/components/layout';
 import { useGetVenuesQuery } from '@/services/holidaze';
+
+import Layout from '@/components/layout';
 import VenueCard from '@/components/venue/venue-card';
+
 import Container from '@/components/common/container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 export default function AllVenuesPage() {
     const { data, error, isLoading } = useGetVenuesQuery('');
@@ -10,17 +14,19 @@ export default function AllVenuesPage() {
         <>
             <Layout>
                 <Container>
-                    <h1> All venues</h1>
+                    <Typography component='h1' variant='h2'>
+                        All venues
+                    </Typography>
                     {error ? (
                         <p>Oh no, there was an error</p>
                     ) : isLoading ? (
                         <p>Loading...</p>
                     ) : data ? (
-                        <ul>
+                        <Grid container component='ul' gap={2} sx={{ listStyleType: 'none', padding: 0 }}>
                             {data.map((venue) => (
                                 <VenueCard key={venue.id} {...venue} />
                             ))}
-                        </ul>
+                        </Grid>
                     ) : null}
                 </Container>
             </Layout>
