5f8b60b7b854eb442ecb9197b1f06c3b15de680b
Joakim Tveter <joakim@tveter.net>
2023-10-18 09:40:02 +0200

    Added toast settings

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
+import Box from '@mui/material/Box';
+
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 
@@ -9,8 +11,21 @@ export default function StandardLayout({ children }: { children: ReactNode }) {
     return (
         <>
             <Header />
-            {children}
-            <ToastContainer />
+            <Box component='main' id='main-content'>
+                {children}
+            </Box>
+            <ToastContainer 
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
             <Footer />
         </>
     );
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,18 +1,32 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
+import { useGetProfileByNameQuery } from '@/services/holidaze';
 import { RootState } from '@/store';
 
 import Container from '@/components/common/container';
 import Layout from '@/components/layout';
-import { useGetProfileByNameQuery } from '@/services/holidaze';
-import { Avatar, Box, IconButton, Stack, Typography } from '@mui/material';
+
+import { Avatar, Box, Chip, IconButton, Stack, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import VenueCard from '@/components/venue/venue-card';
 
 export default function ProfilePage() {
     const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
     const { profileName } = useParams<{ profileName: string }>();
+
+    // If the user is not logged in and is not trying to access his own profile, redirect to login page
     if (!isLoggedIn && !profileName) {
+        toast.info('You are not logged in', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
         const navigate = useNavigate();
         navigate('/sign-in');
     }
@@ -41,23 +55,26 @@ export default function ProfilePage() {
                     ) : isLoading ? (
                         <p>Loading...</p>
                     ) : data ? (
-                        <Box>
-                            <Stack direction='row' alignItems='center' gap={2}>
+                        <Box mt={3}>
+                            <Stack direction='row' alignItems='center' gap={4}>
                             <Avatar
                                 alt={data.name.toLocaleUpperCase()}
-                                src={data.avatar !== '' ? data.avatar : 'this string ensures a letter'}
+                                src={data.avatar}
                                 sx={{ width: 200, height: 200 }}
                             />
-                                <Stack component='hgroup'>
-                                    <Typography component='h1' variant='h3'> {data.name}  
+                                <Stack component='hgroup' spacing={1}>
+                                    <Typography component='h1' variant='h3'> {data.name}
+                                        {isLoggedIn && !profileName &&
                                         <IconButton aria-label='edit profile' component={Link} to={'/profile/edit'}>
                                             <EditIcon />
                                         </IconButton>
+                                        }  
                                     </Typography>
                                     <Typography component='p' variant='subtitle1'> {data.email}</Typography>
+                                    {data.venueManager  && <Chip label="Venue manager" color='primary' sx={{width: 'max-content'}} />}
                                 </Stack>
                             </Stack>
-                            {data.venueManager  && 
+                            {data.venueManager && data._count.venues > 0 &&
                             <Box>
                                 <Typography component='h2' variant='h4'>Venues</Typography>
                                 <Stack component='ul' sx={{listStyleType: 'none', padding: 0}}>
