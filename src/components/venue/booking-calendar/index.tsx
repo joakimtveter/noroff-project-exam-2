a5fc508a9e5653ae78100f9af4f5bc69ddb1431d
Joakim Tveter <joakim@tveter.net>
2023-10-18 08:24:10 +0200

    Adding Booking Calendar

---
+++ src/components/venue/booking-calendar/index.tsx
@@ -0,0 +1,27 @@
+import Box from "@mui/material/Box";
+import {Button, CalendarCell, CalendarGrid, Heading, RangeCalendar} from 'react-aria-components';
+
+type BookingCalendarProps = {
+    bookings: any[]
+}
+
+export default function BookingCalendar(props: BookingCalendarProps) {
+    const {bookings} = props;
+  return (
+    <RangeCalendar aria-label="Trip dates" style={{width: 'max-content'}}>
+    <Box component='header' sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Button slot="previous">◀</Button>
+        <Heading />
+        <Button slot="next">▶</Button>
+    </Box>
+    <Box sx={{ display: 'flex', gap: 2, overflow: 'auto' }}>
+        <CalendarGrid>
+            {(date) => <CalendarCell date={date} />}
+        </CalendarGrid>
+        <CalendarGrid offset={{ months: 1 }}>
+            {(date) => <CalendarCell date={date} />}
+        </CalendarGrid>
+    </Box>
+</RangeCalendar>
+  )
+}
