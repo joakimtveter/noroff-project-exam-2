a415c9afae0c12b4c634e2b8d298d2d8478cc4f2
Joakim Tveter <joakim@tveter.net>
2023-11-07 15:10:26 +0100

    Added Booking to calendar

--- src/components/venue/booking-calendar/index.tsx
+++ src/components/venue/booking-calendar/index.tsx
@@ -1,19 +1,60 @@
-import { Box } from '@mui/material'
-import { I18nProvider } from 'react-aria'
+import { ChangeEvent, ReactElement, useState } from 'react'
+import { getLocalTimeZone, today } from '@internationalized/date'
 import { Button, CalendarCell, CalendarGrid, Heading, RangeCalendar } from 'react-aria-components'
-import { ReactElement } from 'react'
-import { Booking } from '@/types/venue.ts'
+
+import { useCreateBookingMutation } from '@/services/holidaze'
+
+import { Box, Button as MuiButton, InputAdornment, TextField } from '@mui/material'
+import GroupIcon from '@mui/icons-material/Group'
+
+import { Booking } from '@/types/booking.ts'
 
 interface BookingCalendarProps {
     bookings: Booking[]
+    maxGuests: number
+    venueId: string
 }
 
 export default function BookingCalendar(props: BookingCalendarProps): ReactElement {
-    const { bookings } = props
+    const { bookings, maxGuests = 100, venueId } = props
+    const [createBooking] = useCreateBookingMutation()
+    const [dates, setDates] = useState({
+        start: today(getLocalTimeZone()),
+        end: today(getLocalTimeZone()),
+    })
+    const [guests, setGuests] = useState(1)
     console.log('bookings: ', bookings)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const guests = parseInt(e.target.value)
+        if (isNaN(guests)) return
+        if (guests > maxGuests) {
+            setGuests(maxGuests)
+            return
+        }
+        if (guests < 1) {
+            setGuests(1)
+            return
+        }
+        setGuests(guests)
+    }
+
+    const handleClick = async (): Promise<void> => {
+        const dateFrom = new Date(dates.start.toString()).toISOString()
+        const dateTo = new Date(dates.end.toString()).toISOString()
+        console.log('values: ', { dateTo, dateFrom, guests, venueId })
+
+        await createBooking({ dateTo, dateFrom, guests, venueId })
+    }
     return (
-        <I18nProvider locale="en-NO">
-            <RangeCalendar aria-label="Trip dates" style={{ width: 'max-content' }}>
+        <Box sx={{ marginBlock: 4 }}>
+            <RangeCalendar
+                aria-label="Trip dates"
+                value={dates}
+                onChange={setDates}
+                minValue={today(getLocalTimeZone())}
+                visibleDuration={{ months: 2 }}
+                style={{ width: 'max-content' }}
+            >
                 <Box component="header" sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Button slot="previous">◀</Button>
                     <Heading />
@@ -24,6 +65,23 @@ export default function BookingCalendar(props: BookingCalendarProps): ReactEleme
                     <CalendarGrid offset={{ months: 1 }}>{(date) => <CalendarCell date={date} />}</CalendarGrid>
                 </Box>
             </RangeCalendar>
-        </I18nProvider>
+
+            <Box>
+                <TextField
+                    label={'Number of guests'}
+                    variant={'outlined'}
+                    margin={'normal'}
+                    value={guests}
+                    onChange={handleChange}
+                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', style: { textAlign: 'end' } }}
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">{<GroupIcon />}</InputAdornment>,
+                    }}
+                />
+                <MuiButton variant="contained" onClick={handleClick} sx={{ marginBlock: 2 }}>
+                    Book Venue
+                </MuiButton>
+            </Box>
+        </Box>
     )
 }
