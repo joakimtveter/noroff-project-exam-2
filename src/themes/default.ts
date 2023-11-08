aeabf2333013ef0a85fe7a6dac919d20e305ffc2
Joakim Tveter <joakim@tveter.net>
2023-11-08 10:01:11 +0100

    Updated color palette

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,31 +1,17 @@
 import { createTheme } from '@mui/material/styles'
 
-// declare module '@mui/material/styles/createPalette' {
-//     interface TypeText {
-//         green?: string;
-//         purple?: string;
-//     }
-// }
-
 const defaultTheme = createTheme({
     palette: {
+        mode: 'light',
         primary: {
-            dark: '#05445E',
-            main: '#0088A3',
-            light: '#D4F1F4',
+            main: '#008577',
         },
         secondary: {
-            dark: '#00726F',
             main: '#0abab5',
-            light: '#0CE6DF',
         },
         background: {
-            paper: '#E7F7F9',
+            paper: '#E8FAF9',
         },
-        // text: {
-        //     green: '#09725d',
-        //     purple: '#b30f9b',
-        // },
     },
     typography: {
         fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
