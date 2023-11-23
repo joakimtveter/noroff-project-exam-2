d00cc9dc747702122884435d339371193003434b
Joakim Tveter <joakim@tveter.net>
2023-11-23 23:47:16 +0100

    Fixed Booking Dialog

--- src/components/profile/booking-dialog/index.tsx
+++ src/components/profile/booking-dialog/index.tsx
@@ -7,7 +7,9 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Link,
 } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
 import { useGetBookingQuery } from '@/services/holidaze.ts'
 
 interface BookingDialogProps {
@@ -19,6 +21,11 @@ export default function BookingDialog(props: BookingDialogProps) {
     const { handleClose, open, bookingId } = props
     const { data, error, isLoading } = useGetBookingQuery(bookingId)
 
+    const arrivalDate = data ? new Date(data.dateFrom) : null;
+    const departureDate = data ? new Date(data.dateTo) : null;
+    const numberOfNights = departureDate && arrivalDate ? Math.floor((departureDate.setHours(0,0,0,0) - arrivalDate.setHours(0,0,0,0)) / (1000 * 3600 * 24)) : null;
+
+
     if (error) console.error(error)
     return (
         <Dialog
@@ -27,21 +34,25 @@ export default function BookingDialog(props: BookingDialogProps) {
             aria-labelledby="booking-dialog-title"
             aria-describedby="booking-dialog-description"
         >
-            <DialogTitle id="booking-dialog-title">Booking details</DialogTitle>
+            <DialogTitle id="booking-dialog-title">Booking details - {data?.venue.name}</DialogTitle>
             <DialogContent>
                 {isLoading ? (
                     <CircularProgress />
                 ) : (
                     <Box>
-                        <DialogContentText id="booking-dialog-description">tesdt</DialogContentText>
-                        <pre>{JSON.stringify(data, null, 2)}</pre>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Arrive on: </Box>{arrivalDate?.toLocaleDateString('en-NO', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'})}</DialogContentText>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Depart on: </Box>{departureDate?.toLocaleDateString('en-NO', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'})}</DialogContentText>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Number of nights: </Box>{numberOfNights}</DialogContentText>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Number of guests: </Box>{data?.guests}</DialogContentText>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Price per night: </Box>{data?.venue.price}</DialogContentText>
+                        <DialogContentText id="booking-dialog-description"><Box component='span' sx={{fontWeight: 700}}>Total price: </Box>{(data?.venue.price ?? 0) * (numberOfNights ?? 0)}</DialogContentText>
+                        <Link component={RouterLink} to={`/venues/${data?.venue.id}`} underline="hover"> Visit venue page</Link>
                     </Box>
                 )}
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Disagree</Button>
                 <Button onClick={handleClose} autoFocus>
-                    Agree
+                    Dismiss
                 </Button>
             </DialogActions>
         </Dialog>
