6318a901d6285effe31881c37787ecdc5faeae26
Joakim Tveter <joakim@tveter.net>
2023-11-26 19:51:04 +0100

    Update title and alt text

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,19 +1,23 @@
 import { ReactElement } from 'react'
+import { Helmet } from 'react-helmet'
 import { Box, Grid, Paper } from '@mui/material'
-import image from '@/assets/juan-burgos-_m3nD5aQIDo-unsplash.webp'
 
 import LoginForm from '@/components/forms/sign-in-form'
+import image from '@/assets/juan-burgos-_m3nD5aQIDo-unsplash.webp'
 
 export default function LoginPage(): ReactElement {
     return (
         <Box sx={{display: 'grid', placeItems: 'center', padding: 2, height: '100%'}}>
+            <Helmet>
+                <title>Sign in | Holidaze</title>
+            </Helmet>
             <Paper component='section' elevation={2} sx={{marginBlock: 4}}>
                 <Grid container>
                     <Grid item xs={12} md={6}>
                         <Box component='figure' sx={{aspectRatio: '1 / 1', }}>
                             <img 
                                 src={image} 
-                                alt='A nice white beach with two sunbeds and palmtrees. The skye is blue, and the water is nice and clear' 
+                                alt='A beautifull morning in the mountains. The moon is still in the sky while the lower part of the sky is turning yellow. A pool and palmtrees are in the forground and a town is down in a vally in the background.' 
                                 style={{objectFit: 'cover', height: '100%', width: '100%'}} />
                         </Box>
                     </Grid>
--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,20 +1,23 @@
 import { ReactElement } from 'react'
+import { Helmet } from 'react-helmet'
 import { Box, Grid, Paper } from '@mui/material'
 
-import image from '@/assets/mohamed-thasneem-3bAblStd-IY-unsplash.webp'
-
 import SignUpForm from '@/components/forms/sign-up-form'
+import image from '@/assets/mohamed-thasneem-3bAblStd-IY-unsplash.webp'
 
 export default function SignUpPage(): ReactElement {
     return (
         <Box sx={{display: 'grid', placeItems: 'center', padding: 2, height: '100%'}}>
+            <Helmet>
+                <title>Sign up | Holidaze</title>
+            </Helmet>
             <Paper component='section' elevation={2}>
                 <Grid container>
                     <Grid item xs={12} md={6} order={{xs: 2, md: 1}}>
                         <Box component='figure' sx={{aspectRatio: '1 / 1', }}>
                             <img 
                                 src={image} 
-                                alt='A nice white beach with two sunbeds and palmtrees. The skye is blue, and the water is nice and clear' 
+                                alt='A beautiful sunset over a pool. The puffy clouds and palmtrees reflect perfectly in the still water. A row of bungalows and the ocean are visible in the background.' 
                                 style={{objectFit: 'cover', height: '100%', width: '100%'}} />
                         </Box>
                     </Grid>
